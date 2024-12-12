ea228ea5217d4dc944fc296dc4cba948faec4678
Rob Bierman <rbierman@princeton.edu>
2024-12-12 11:24:09 -0500

    chore: Add day12.1 to lib.rs. Too slow need refact

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,11 @@ fn day11_part2(data_path: &str) -> PyResult<u64> {
     Ok(days::day11::part2(Path::new(data_path)))
 }
 
+#[pyfunction]
+fn day12_part1(data_path: &str) -> PyResult<u32> {
+    Ok(days::day12::part1(Path::new(data_path)))
+}
+
 #[pymodule]
 fn advent2024(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(day1_part1, m)?)?;
@@ -137,5 +142,6 @@ fn advent2024(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(day10_part2, m)?)?;
     m.add_function(wrap_pyfunction!(day11_part1, m)?)?;
     m.add_function(wrap_pyfunction!(day11_part2, m)?)?;
+    m.add_function(wrap_pyfunction!(day12_part1, m)?)?;
     Ok(())
 }
