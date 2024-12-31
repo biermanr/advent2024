cdb4ae2c11adf8c67283fe01de80f7fc33989f6f
Rob Bierman <rbierman@princeton.edu>
2024-12-31 16:19:13 -0500

    fix: Fixed run-all

--- python/advent2024/run_all_days.py
+++ python/advent2024/run_all_days.py
@@ -16,28 +16,27 @@ def main():
     args = parser.parse_args()
 
     days = [
-        advent2024.day1_part1, advent2024.day1_part2,
-        advent2024.day2_part1, advent2024.day2_part2,
-        advent2024.day3_part1, advent2024.day3_part2,
-        advent2024.day4_part1, advent2024.day4_part2,
-        advent2024.day5_part1, advent2024.day5_part2,
-        advent2024.day6_part1, advent2024.day6_part2,
-        advent2024.day7_part1, advent2024.day7_part2,
-        advent2024.day8_part1, advent2024.day8_part2,
-        advent2024.day9_part1, advent2024.day9_part2,
-        advent2024.day10_part1, advent2024.day10_part2,
-        advent2024.day11_part1, advent2024.day11_part2,
-        advent2024.day12_part1, advent2024.day12_part2,
-        advent2024.day13_part1, advent2024.day13_part2,
+        (1, [advent2024.day1_part1, advent2024.day1_part2]),
+        (2, [advent2024.day2_part1, advent2024.day2_part2]),
+        (3, [advent2024.day3_part1, advent2024.day3_part2]),
+        (4, [advent2024.day4_part1, advent2024.day4_part2]),
+        (5, [advent2024.day5_part1, advent2024.day5_part2]),
+        (6, [advent2024.day6_part1, advent2024.day6_part2]),
+        (7, [advent2024.day7_part1, advent2024.day7_part2]),
+        (8, [advent2024.day8_part1, advent2024.day8_part2]),
+        (9, [advent2024.day9_part1, advent2024.day9_part2]),
+       (10, [advent2024.day10_part1, advent2024.day10_part2]),
+       (11, [advent2024.day11_part1, advent2024.day11_part2]),
+       (12, [advent2024.day12_part1, advent2024.day12_part2]),
+       (13, [advent2024.day13_part1, advent2024.day13_part2]),
+       (25, [advent2024.day25_part1]),
     ]
 
-    for i, func in enumerate(days):
-        day = i//2 + 1
-        part = i%2 + 1
-
-        with timer():
-            result = func(f"{args.data}/day{day}.txt")
-            print(f"Day {day} part {part} result {result}", end=" ")
+    for day, funcs in days:
+        for part,func in enumerate(funcs):
+            with timer():
+                result = func(f"{args.data}/day{day}.txt")
+                print(f"Day {day} part {part} result {result}", end=" ")
 
 if __name__ == "__main__":
     main()
